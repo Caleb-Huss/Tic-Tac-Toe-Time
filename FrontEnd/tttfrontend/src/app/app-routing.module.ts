c80827c34b792b7232d7928feda571d85a4bbcf5
Caleb-Huss <caleb.huss97@gmail.com>
2021-09-14 11:29:03 -0500

    redid routing

--- FrontEnd/tttfrontend/src/app/app-routing.module.ts
+++ FrontEnd/tttfrontend/src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameSquaresComponent } from './components/game-squares/game-squares.component';
+import { GameComponent } from './components/game/game.component';
+import { HomeComponent } from './components/home/home.component';
+import { MakeroomComponent } from './components/makeroom/makeroom.component';
+import { RoomsComponent } from './components/rooms/rooms.component';
+import { RulesComponent } from './components/rules/rules.component';
+import { StatsComponent } from './components/stats/stats.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path: '',component: HomeComponent},
+  {path: 'makeroom',component: MakeroomComponent},
+  {path: 'game',component: GameComponent},
+  {path: 'gamesquares',component: GameSquaresComponent},
+  {path: 'rooms',component: RoomsComponent},
+  {path: 'stats',component: StatsComponent},
+  {path: 'rules',component: RulesComponent}
+];
+// {path: ,component: },
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
